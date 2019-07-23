42d1f3a3cd06211ae0578202f0bd1ecc9ca688ec
frank mcmanus <frank@thefrankharvey.com>
2019-07-23 10:17:21 -0400

    initialized barebones gatsby template
    tested and works locally

---
+++ node_modules/gatsby/dist/utils/worker/pool.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const Worker = require(`jest-worker`).default;
+
+const cpuCoreCount = require(`./cpu-core-count`);
+
+const create = () => new Worker(require.resolve(`./child`), {
+  numWorkers: cpuCoreCount(true),
+  forkOptions: {
+    silent: false
+  }
+});
+
+module.exports = {
+  create
+};
+//# sourceMappingURL=pool.js.map
