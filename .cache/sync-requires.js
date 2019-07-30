30b31c4c7be515906ed246229ce83667b4467d55
Frank <frank@thefrankharvey.com>
2019-07-30 16:08:03 -0400

    cleared default template components

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,7 +7,6 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/frank.mcmanus/Documents/_development/Typescript/website/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/frank.mcmanus/Documents/_development/Typescript/website/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/frank.mcmanus/Documents/_development/Typescript/website/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/frank.mcmanus/Documents/_development/Typescript/website/src/pages/page-2.js")))
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/frank.mcmanus/Documents/_development/Typescript/website/src/pages/index.js")))
 }
 
