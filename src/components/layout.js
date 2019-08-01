c4100a07d3b239f98d457f38e513b4c6a21b8d96
Frank <frank@thefrankharvey.com>
2019-08-01 14:17:42 -0400

    layout renders components locally

---
+++ src/components/layout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import HomeNav from "./HomeNav";
+
+class Layout extends React.Component {
+
+  render() {
+    return (
+      <div className='app'>
+      <Header />
+      {/* if statements here to render content depending on click event listener */}
+      {/* the props sent to HomeNav will come from meetup api */}
+      <HomeNav content={this.props}/>
+      <Footer/>
+      </div>
+    )
+  }
+}
+
+export default Layout
