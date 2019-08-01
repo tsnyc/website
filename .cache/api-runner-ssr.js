542e48b883af493c28213651c1eb96c17a456561
Frank <frank@thefrankharvey.com>
2019-08-01 14:14:40 -0400

    updated gatsby config files to run app locally

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -3,7 +3,7 @@ var plugins = [{
       options: {"plugins":[]},
     },{
       plugin: require('/Users/frank.mcmanus/Documents/_development/Typescript/website/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png"},
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
