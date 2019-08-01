a802ca47e6bf6bc3777d4d0354c96d4cf2eef6cf
Frank <frank@thefrankharvey.com>
2019-08-01 14:15:47 -0400

    add basic header component data

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+class Header extends React.Component {
+
+  render() {
+    return (
+      <div className="header">
+        <Link to="/"><h1>I am a header</h1></Link>
+      </div>
+    )
+  }
+}
+
+export default Header
