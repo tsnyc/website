b5aa472060cb14ce17610c74417e15a0b1ed0e1f
Frank <frank@thefrankharvey.com>
2019-08-12 15:34:12 -0400

    Minor changes to header and footer components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+
 class Footer extends React.Component {
 
   render() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+
 class Header extends React.Component {
 
   render() {
