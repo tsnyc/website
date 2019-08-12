f7522841f54525160223a79614b3cf364c708d01
Frank <frank@thefrankharvey.com>
2019-08-12 15:32:51 -0400

    Add basic component structure to code for conduct

---
+++ src/components/CodeConduct.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+class CodeConduct extends React.Component {
+
+  render() {
+    return (
+      <div className="code-conduct">
+        <Link to="/"><h1>I am a CodeConduct</h1></Link>
+      </div>
+    )
+  }
+}
+
+export default CodeConduct
