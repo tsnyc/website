4fa0aa0ae59c4d2b0b18532e5a00e90a9302b0a3
frank mcmanus <frank@thefrankharvey.com>
2019-08-25 20:38:08 -0400

    Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Typescript NYC meetup group`,
+    description: `Welcome to the home of Typescript NYC`,
+    author: `@thefrankharvey`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
       },
     },
     `gatsby-transformer-sharp`,
