d12e91a6ba202079870dec868360de2b59d01ff1
frank mcmanus <frank@thefrankharvey.com>
2019-09-28 12:44:14 -0400

    Config Typescript for Gatsby

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -8,9 +8,9 @@ exports.components = {
   "component---src-pages-code-of-conduct-js": () => import("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/code-of-conduct.js" /* webpackChunkName: "component---src-pages-code-of-conduct-js" */),
   "component---src-pages-faq-js": () => import("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/faq.js" /* webpackChunkName: "component---src-pages-faq-js" */),
   "component---src-pages-index-js": () => import("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-past-meetups-js": () => import("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/past-meetups.js" /* webpackChunkName: "component---src-pages-past-meetups-js" */),
   "component---src-pages-meetup-1-js": () => import("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/meetup1.js" /* webpackChunkName: "component---src-pages-meetup-1-js" */),
   "component---src-pages-meetup-2-js": () => import("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/meetup2.js" /* webpackChunkName: "component---src-pages-meetup-2-js" */),
-  "component---src-pages-meetup-3-js": () => import("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/meetup3.js" /* webpackChunkName: "component---src-pages-meetup-3-js" */)
+  "component---src-pages-meetup-3-js": () => import("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/meetup3.js" /* webpackChunkName: "component---src-pages-meetup-3-js" */),
+  "component---src-pages-past-meetups-js": () => import("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/past-meetups.js" /* webpackChunkName: "component---src-pages-past-meetups-js" */)
 }
 
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -11,9 +11,9 @@ exports.components = {
   "component---src-pages-code-of-conduct-js": hot(preferDefault(require("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/code-of-conduct.js"))),
   "component---src-pages-faq-js": hot(preferDefault(require("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/faq.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/index.js"))),
-  "component---src-pages-past-meetups-js": hot(preferDefault(require("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/past-meetups.js"))),
   "component---src-pages-meetup-1-js": hot(preferDefault(require("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/meetup1.js"))),
   "component---src-pages-meetup-2-js": hot(preferDefault(require("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/meetup2.js"))),
-  "component---src-pages-meetup-3-js": hot(preferDefault(require("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/meetup3.js")))
+  "component---src-pages-meetup-3-js": hot(preferDefault(require("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/meetup3.js"))),
+  "component---src-pages-past-meetups-js": hot(preferDefault(require("/Users/frankmcmanus/Documents/_dev/Typescript/website/src/pages/past-meetups.js")))
 }
 
