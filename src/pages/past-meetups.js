bc92445956c04e01b1f5f32a83185ee69cf450d0
frank mcmanus <frank@thefrankharvey.com>
2019-10-12 14:47:22 -0400

    reverting from ts to js

---
+++ src/pages/past-meetups.js
@@ -0,0 +1,12 @@
+import React from "react"
+// import Layout from "../components/layout"
+import MeetupList from "../components/MeetupList"
+
+
+const meetupList = () => (
+  <MeetupList>
+   
+  </MeetupList>
+)
+
+export default meetupList
