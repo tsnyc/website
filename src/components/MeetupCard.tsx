e20782f4758cd8a1f2f79755e7f9e4d680d2cbd4
frank mcmanus <frank@thefrankharvey.com>
2019-10-12 11:22:09 -0400

    add styled components package

--- src/components/MeetupCard.tsx
+++ src/components/MeetupCard.tsx
@@ -1,19 +1,18 @@
 import { Link } from "gatsby";
 import React from "react";
 
-
-const MeetupCard = () => (
-  <div className="meetup-card">
-    <Link to="/meetup1">
-      <h1>I am MeetupCard to meetup1</h1>
-    </Link>
-    <Link to="/meetup2">
-      <h1>I am MeetupCard to meetup2</h1>
-    </Link>
-    <Link to="/meetup3">
-      <h1>I am MeetupCard to meetup3</h1>
-    </Link>
-  </div>
-)
-
-export default MeetupCard;
+export default function MeetupCard() {
+  return (
+    <div className="meetup-card">
+      <Link to="/meetup1">
+        <h1>I am MeetupCard to meetup1</h1>
+      </Link>
+      <Link to="/meetup2">
+        <h1>I am MeetupCard to meetup2</h1>
+      </Link>
+      <Link to="/meetup3">
+        <h1>I am MeetupCard to meetup3</h1>
+      </Link>
+    </div>
+  )
+}
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,16 +4,11 @@ import Header from "./Header";
 import HomeNav from "./HomeNav";
 
 export default function Layout() {
-  const state = useState("");
-  const text = state[0];
-  const setText = state[1];
-
   return (
-  <div className="app">
+  <div className="container">
     <Header />
     {/* the props sent to HomeNav will come from meetup api */}
     <HomeNav />
-      <input  type="text" value = {text} onChange={e => setText(e.target.value)}/>
     <Footer />
   </div>
   )
