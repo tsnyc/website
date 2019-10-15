79bc0e4f06ced06dc06914082315cb7c8bb1105d
frank mcmanus <frank@thefrankharvey.com>
2019-10-15 09:27:46 -0400

    test feature detection for flexbox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
 import Layout from "../components/layout";
-import React from "react";
+import React, { useState } from "react";
 
 
-console.log(window);
 
+// console.log(window.chrome);
 export default function IndexPage() {
+const [state, setState] = useState({
+    flex: false
+});
+// const setFlex = state[1];
+
+const getPrev = prevState => console.log(setState(prevState));
+
     return ( 
-    <Layout></Layout>
+    <Layout>{getPrev()}</Layout>
+   
     )
 }
 
