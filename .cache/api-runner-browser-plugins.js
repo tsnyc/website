1a62871d5d61778faf7acaa9d852013aa34d36a5
Frank <frank@thefrankharvey.com>
2019-11-20 14:49:33 -0500

    Add dependcies to support mdx files

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('/Users/frank.mcmanus/Documents/_development/Typescript/website/node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
       plugin: require('/Users/frank.mcmanus/Documents/_development/Typescript/website/gatsby-browser.js'),
       options: {"plugins":[]},
     }]
--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -4,6 +4,9 @@ var plugins = [{
     },{
       plugin: require('/Users/frank.mcmanus/Documents/_development/Typescript/website/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui"},
+    },{
+      plugin: require('/Users/frank.mcmanus/Documents/_development/Typescript/website/node_modules/gatsby-plugin-mdx/gatsby-ssr'),
+      options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
